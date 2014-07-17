a43269e2115f66a4f6d6215d5d273d605c7a09cb
Dave Zych <dave@davidzych.com>
2014-07-17 10:26:52 -0700

    Created skeleton windsor registration

---
+++ src/Microsoft.Framework.DependencyInjection.Windsor/WindsorRegistration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Castle.Windsor;
+
+namespace Microsoft.Framework.DependencyInjection.Windsor
+{
+    public static class WindsorRegistration
+    {
+        public static void Populate(IWindsorContainer container, IEnumerable<IServiceDescriptor> services, IServiceProvider fallbackProvider)
+        {
+
+        }
+    }
+}

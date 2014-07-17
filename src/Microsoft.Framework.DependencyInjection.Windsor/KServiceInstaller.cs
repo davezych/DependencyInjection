ca92066d275bcdcf6a9702631a6320556a62f803
Dave Zych <dave@davidzych.com>
2014-07-17 10:43:38 -0700

    Added skeleton installer

---
+++ src/Microsoft.Framework.DependencyInjection.Windsor/KServiceInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using System.Collections.Generic;
+
+namespace Microsoft.Framework.DependencyInjection.Windsor
+{
+    public class KServiceInstaller : IWindsorInstaller
+    {
+        private IEnumerable<IServiceDescriptor> _services;
+
+        public KServiceInstaller(IEnumerable<IServiceDescriptor> services)
+	    {
+            _services = services;
+	    }
+
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

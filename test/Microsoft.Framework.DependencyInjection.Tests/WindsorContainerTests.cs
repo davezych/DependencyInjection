46ec8d103544a56ab44f9f658f05368c4292ce51
Dave Zych <dave@davidzych.com>
2014-07-17 11:55:59 -0700

    Added windsor test

---
+++ test/Microsoft.Framework.DependencyInjection.Tests/WindsorContainerTests.cs
@@ -0,0 +1,23 @@
+﻿using Castle.Windsor;
+using Microsoft.Framework.DependencyInjection.Tests.Fakes;
+using Microsoft.Framework.DependencyInjection.Windsor;
+using System;
+
+namespace Microsoft.Framework.DependencyInjection.Tests
+{
+    public class WindsorContainerTests : ScopingContainerTestBase
+    {
+        protected override IServiceProvider CreateContainer()
+        {
+            return CreateContainer(new FakeFallbackServiceProvider());
+        }
+
+        protected override IServiceProvider CreateContainer(IServiceProvider fallbackProvider)
+        {
+            var container = new WindsorContainer();
+            WindsorRegistration.Populate(container, TestServices.DefaultServices(), fallbackProvider);
+
+            return container.Resolve<IServiceProvider>();
+        }
+    }
+}

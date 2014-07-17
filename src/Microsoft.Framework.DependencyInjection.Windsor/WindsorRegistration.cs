68e2b0b248785bef7082d306a00e084605b55b25
Dave Zych <dave@davidzych.com>
2014-07-17 10:40:21 -0700

    Added service scope factory and service scope

--- src/Microsoft.Framework.DependencyInjection.Windsor/WindsorRegistration.cs
+++ src/Microsoft.Framework.DependencyInjection.Windsor/WindsorRegistration.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Castle.Windsor;
 using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.Lifestyle;
 
 namespace Microsoft.Framework.DependencyInjection.Windsor
 {
@@ -25,6 +26,42 @@ namespace Microsoft.Framework.DependencyInjection.Windsor
             public object GetService(Type serviceType)
             {
                 return _container.Resolve(serviceType);
+            }
+        }
+
+        private class WindsorServiceScopeFactory : IServiceScopeFactory
+        {
+            private readonly IWindsorContainer _container;
+
+            public WindsorServiceScopeFactory(IWindsorContainer container)
+            {
+                _container = container;
+            }
+
+            public IServiceScope CreateScope()
+            {
+                return new WindsorServiceScope(_container);
+            }
+        }
+
+        private class WindsorServiceScope : IServiceScope
+        {
+            private readonly IWindsorContainer _container;
+            private readonly IServiceProvider _serviceProvider;
+
+            public WindsorServiceScope(IWindsorContainer container)
+            {
+                _container = container;
+                _serviceProvider = _container.Resolve<IServiceProvider>();
+            }
+
+            public IServiceProvider ServiceProvider
+            {
+                get { return _serviceProvider; }
+            }
+
+            public void Dispose()
+            {
             }
         }
     }

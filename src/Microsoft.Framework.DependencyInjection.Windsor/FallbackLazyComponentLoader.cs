2f16f51c33755e97cac292ece32ad934ea7f37a9
Dave Zych <dave@davidzych.com>
2014-07-17 11:05:42 -0700

    Created skeleton fallback component loader

---
+++ src/Microsoft.Framework.DependencyInjection.Windsor/FallbackLazyComponentLoader.cs
@@ -0,0 +1,22 @@
+﻿using Castle.MicroKernel.Resolvers;
+using System;
+using Castle.MicroKernel.Registration;
+using System.Collections;
+
+namespace Microsoft.Framework.DependencyInjection.Windsor
+{
+    public class FallbackLazyComponentLoader : ILazyComponentLoader
+    {
+        private IServiceProvider _fallbackProvider;
+
+        public FallbackLazyComponentLoader(IServiceProvider provider)
+	    {
+            _fallbackProvider = provider;
+	    }
+
+        public IRegistration Load(string name, Type service, IDictionary arguments)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

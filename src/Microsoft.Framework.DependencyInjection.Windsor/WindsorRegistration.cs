feb9863de4ca1b4f06cfb6b6a2ccbcbfbafec7df
Dave Zych <dave@davidzych.com>
2014-07-17 10:29:31 -0700

    Added WindsorServiceProvider

--- src/Microsoft.Framework.DependencyInjection.Windsor/WindsorRegistration.cs
+++ src/Microsoft.Framework.DependencyInjection.Windsor/WindsorRegistration.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Castle.Windsor;
+using Castle.MicroKernel.Registration;
 
 namespace Microsoft.Framework.DependencyInjection.Windsor
 {
@@ -8,7 +9,23 @@ namespace Microsoft.Framework.DependencyInjection.Windsor
     {
         public static void Populate(IWindsorContainer container, IEnumerable<IServiceDescriptor> services, IServiceProvider fallbackProvider)
         {
+            container.Register(Component.For<IWindsorContainer>().Instance(container));
+            container.Register(Component.For<IServiceProvider>().ImplementedBy<WindsorServiceProvider>());
+        }
 
+        public class WindsorServiceProvider : IServiceProvider
+        {
+            private IWindsorContainer _container;
+
+            public WindsorServiceProvider(IWindsorContainer container)
+            {
+                _container = container;
+            }
+
+            public object GetService(Type serviceType)
+            {
+                return _container.Resolve(serviceType);
+            }
         }
     }
 }
